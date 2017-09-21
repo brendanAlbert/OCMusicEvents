220bb48226fb88861321b1330ef465bf38f0338b
brendanAlbert <alber123@mail.chapman.edu>
2017-09-21 16:44:35 -0700

    - Typed up JavaDocs. - Added assets folder.

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/balbert/ocmusicevents/EventDetailsActivity.java
@@ -0,0 +1,85 @@
+package edu.orangecoastcollege.cs273.balbert.ocmusicevents;
+
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * EventDetailsActivity is a class which represents the second Controller of OC Music Events.
+ * This Controller establishes connections to the widgets of its View, activity_event_details.xml.
+ *
+ * We instantiate and link up the various TextViews and ImageView.
+ * We get the Intent provided from EventsListActivity and populate the appropriate widgets.
+ *
+ * An AssetManager is utilized to provide links to the appropriate images in the assets folder.
+ *
+ * When the user wants to return to the list, a button is provided that calls goBackToList,
+ * which cleans up the memory and returns to the prior activity, EventListActivity.
+ */
+public class EventDetailsActivity extends AppCompatActivity {
+
+    /**
+     * onCreate() is the work horse of this Class.
+     * We set the content view from the respective View, activity_event_details.xml.
+     * We create connections to the ImageView, Button and TextViews.
+     *
+     * An AssetManager is used to load the appropriate image, from the assets folder,
+     * for the list item that was tapped.
+     *
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_details);
+
+        // Get the data out of the Intent
+        Intent intent = getIntent();
+        String title = intent.getStringExtra("title");
+        String details = intent.getStringExtra("details");
+
+        // Create references to the text views
+        TextView titleTextView = (TextView) findViewById(R.id.eventTitleTextView);
+        TextView detailsTextView = (TextView) findViewById(R.id.eventDetailsTextView);
+        ImageView eventImageView = (ImageView) findViewById(R.id.eventImageView);
+
+        // Set the text of the text views
+        titleTextView.setText(title);
+        detailsTextView.setText(details);
+
+        // Use the Asset Manager to retrieve a file (image)
+        AssetManager am = this.getAssets();
+        String imageFileName = title.replace(" ", "") + ".jpeg";
+        // Use AssetManager to open a stream to the file name
+        try {
+            InputStream stream = am.open(imageFileName);
+            Drawable image = Drawable.createFromStream(stream, title);
+            eventImageView.setImageDrawable(image);
+        } catch (IOException e) {
+            Log.e("OC Music Events", "Error loading image: " + imageFileName, e);
+        }
+    }
+
+    /**
+     *
+     * @param v is the View that called goBackToList
+     *
+     *          goBackToList is the method called when the user taps BACK TO LIST.
+     *          Inside we call finish() to clean up the memory and return to the previous activity.
+     *
+     */
+    protected void goBackToList(View v)
+    {
+        // Terminates the current activity ( DetailsActivity )
+        finish();
+    }
+}
--- app/src/main/java/edu/orangecoastcollege/cs273/balbert/ocmusicevents/EventsListActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/balbert/ocmusicevents/EventsListActivity.java
@@ -1,13 +1,65 @@
 package edu.orangecoastcollege.cs273.balbert.ocmusicevents;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class EventsListActivity extends AppCompatActivity {
 
+/**
+ * EventsListActivity is a class which represents one of the two Controllers.
+ * This is the Controller for the first View the user sees, activity_events_list.xml.
+ *
+ *  It contains two methods, onCreate and onListItemClick.
+ *
+ *  onCreate(Bundle) remembers the previous state if the user minimizes our app.
+ *  Inside we create a ListAdapter to display our event titles.
+ *
+ *  onListItemClick() is called when an item in the list is selected.
+ */
+public class EventsListActivity extends ListActivity {
+
+    /**
+     * We use onCreate to create a ListAdapter.
+     * We are using a default built-in ListAdapter.
+     * This populates the list with the titles of our music events.
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_events_list);
+        // Content has already been set for ListActivity
+        //setContentView(R.layout.activity_events_list);
+
+        // Define a built-in list adapter for this ListActivity:
+        setListAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, MusicEvent.titles));
+    }
+
+    /**
+     * onListItemClick is a method we get for free by extending ListActivity. We have to
+     * fill in the implementation details however.
+     * @param l
+     * @param v
+     * @param position for our purposes, position is the only parameter used.
+     *                 Position represents the element tapped.
+     *                 We use this position to connect to the appropriate title and detail
+     *                 from the Model.  These are set up as parallel String arrays.
+     * @param id
+     */
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+
+        // Use the position in the list to look up the position in the titles array.
+        String title = MusicEvent.titles[position];
+        String details = MusicEvent.details[position];
+
+        // Create an Intent to go to DetailsActivity with the respective
+        // title and details.
+        Intent detailsIntent = new Intent(this, EventDetailsActivity.class);
+        detailsIntent.putExtra("title", title);
+        detailsIntent.putExtra("details", details);
+        startActivity(detailsIntent);
     }
 }
